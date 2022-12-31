d45660ec26e466fb6e82c8e054e1a9ad10681f16
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2022-12-31 15:49:19 -0300

    Add mobile menu first version

---
+++ build/js/app.js
@@ -0,0 +1,14 @@
+const initApp = () => {
+  const hamburguerBtn = document.getElementById('hamburguer-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle('hidden');
+    mobileMenu.classList.toggle('flex');
+  };
+
+  hamburguerBtn.addEventListener('click', toggleMenu);
+  mobileMenu.addEventListener('click', toggleMenu);
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
